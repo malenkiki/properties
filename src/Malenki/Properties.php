f6508df6d80e7e6dd759212b73d8bd3b25728d14
malenkiki <petit.michel@gmail.com>
2013-10-30 23:19:24 +0100

    Indent

--- src/Malenki/Properties.php
+++ src/Malenki/Properties.php
@@ -21,6 +21,9 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+
+
 namespace Malenki;
 
 
@@ -30,67 +33,88 @@ namespace Malenki;
  */
 class Properties
 {
-    private $file          = null;
-    private $keyValue      = array();
-    private $newLine       = "\n";
-    private $separator     = '=';
-    private $withMultiline = false;
+    private $str_file      = null;
+    private $arr_key_value = array();
+    private $str_new_line  = "\n";
+    private $str_separator = '=';
+    private $is_multiline  = false;
+
+
 
     /**
      * @brief Le constructeur.
-     * @param $file Le nom du fichier à lire
+     * @param $str_file Le nom du fichier à lire
      */
-    public function __construct($file){
-        if(file_exists($file) and !is_dir($file)){
-            $this->file = $file;
+    public function __construct($str_file)
+    {
+        if(file_exists($str_file) && !is_dir($str_file))
+        {
+            $this->str_file = $str_file;
         }
     }
 
+
+
     /**
      * @brief Indique quel est le retour à la ligne utilisé
-     * @param $newLine @c String indiquant le retour 
+     * @param $str @c String indiquant le retour 
      * ("\r","\r\n" ou "\n")
      * @see setNewLineAsDOS(), setNewLineAsUNIX() et 
      * setNewLineAsMAC()
      */
-    private function setNewLine($newLine){
-        $this->newLine = $newLine;
+    private function setNewLine($str)
+    {
+        $this->str_new_line = $str;
     }
+
+
 
     /**
      * @brief Indique qu'il faut utiliser et prendre en compte 
      * les retours à la
      * ligne de type DOS/Windows
      */
-    public function setNewLineAsDOS(){
+    public function setNewLineAsDOS()
+    {
         $this->setNewLine("\r\n");
     }
+
+
 
     /**
      * @brief Indique qu'il faut utiliser et prendre en compte 
      * les retours à la
      * ligne de type UNIX
      */
-    public function setNewLineAsUNIX(){
+    public function setNewLineAsUNIX()
+    {
         $this->setNewLine("\n");
     }
+
+
 
     /**
      * @brief Indique qu'il faut utiliser et prendre en compte 
      * les retours à la
      * ligne de type MAC
      */
-    public function setNewLineAsMAC(){
+    public function setNewLineAsMAC()
+    {
         $this->setNewLine("\r");
     }
+
+
 
     /**
      * @brief Utilisation de valeurs ayant des retours à la 
      * ligne en leur sein
      */
-    public function useMultilineValues(){
-        $this->withMultiline = true;
+    public function multiLine()
+    {
+        $this->is_multiline = true;
     }
+
+
 
     /**
      * @brief Utilisation de valeurs sans retour à la ligne
@@ -99,76 +123,102 @@ class Properties
      * valeur après le premier
      * retour à la ligne est ignoré.
      */
-    public function useOneLineValues(){
-        $this->withMultiline = false;
+    public function singleLine()
+    {
+        $this->is_multiline = false;
     }
+
+
 
     /**
      * @brief Parcourt le fichier et stocke les valeurs trouvées
      * @return Boolean
      */
-    public function readContent(){
-        if(!is_null($this->file)){
-            $content = file_get_contents($this->file);
-            $content = explode($this->newLine,$content);
+    public function read()
+    {
+        if(!is_null($this->str_file))
+        {
+            $content = file_get_contents($this->str_file);
+            $content = explode($this->str_new_line,$content);
 
             // On parcourt le tableau qui contient les lignes...
-            foreach($content as $line){
+            foreach($content as $line)
+            {
                 // Si on trouve un signe "égal", alors on s'attend à trouver 2 parties
-                if(strpos($line,$this->separator) !== false){
-                    list($key,$value) = explode($this->separator,$line);
+                if(strpos($line,$this->str_separator) !== false)
+                {
+                    list($key,$value) = explode($this->str_separator,$line);
                     $key   = trim($key);
                     $value = trim($value);
 
                     // On vérifie que la clé est non nulle, si nulle, est ignorée
                     if(strlen($key) > 0)
-                        $this->keyValue[$key] = $value;
+                    {
+                        $this->arr_key_value[$key] = $value;
+                    }
 
                     // On stocke la dernière clé pour le cas des retours à la ligne dans
                     // les valeurs (cf. en-dessous)
                     $lastKey = $key;
-                } else {
-                    if($this->withMultiline){
+                }
+                else
+                {
+                    if($this->is_multiline)
+                    {
                         // On est dans le cas où il n'y a pas de signe "égal"
                         $line = trim($line);
 
                         // Si la ligne contient quelquechose, et qu'au moins une clé est déjà
                         // stockée, alors on concataine cette ligne à la valeur de la clé
                         // précédente
-                        if(strlen($line) > 0 and count($this->keyValue) > 0){
-                            $this->keyValue[$lastKey] .= $this->newLine.$line;
+                        if(strlen($line) > 0 and count($this->arr_key_value) > 0)
+                        {
+                            $this->arr_key_value[$lastKey] .= $this->str_new_line.$line;
                         }
                     }
                 }
             }
 
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
+
+
 
     /**
      * @brief Écrit les valeurs dans le fichier.
      */
-    public function writeContent(){
-        if(!is_null($this->file)){
+    public function save()
+    {
+        if(!is_null($this->str_file))
+        {
             $str = '';
 
-            foreach($this->keyValue as $k => $v){
-                $str .= $k . $this->separator . $v . $this->newLine;
+            foreach($this->arr_key_value as $k => $v)
+            {
+                $str .= $k . $this->str_separator . $v . $this->str_new_line;
             }
 
-            $result = file_put_contents($this->file, $str);
+            $result = file_put_contents($this->str_file, $str);
 
             if($result === false)
+            {
                 return false;
+            }
 
             return true;
-        } else {
+        } 
+        else 
+        {
             return false;
         }
     }
+
+
 
     /**
      * @brief Retourne les valeurs trouvées
@@ -178,11 +228,17 @@ class Properties
      *
      * @return mixed
      */
-    public function getAllValues(){
-        if(count($this->keyValue) == 0)
+    public function getAllValues()
+    {
+        if(count($this->arr_key_value) == 0)
+        {
             return null;
-        return $this->keyValue;
+        }
+
+        return $this->arr_key_value;
     }
+
+
 
     /**
      * @brief Retourne une valeur donnée.
@@ -190,11 +246,17 @@ class Properties
      * Si le tableau est vide ou que la clé n'existe pas, 
      * retourne @c false.
      */
-    public function getValue($key){
-        if(!array_key_exists($key,$this->keyValue) or count($this->keyValue) == 0)
+    public function getValue($key)
+    {
+        if(!array_key_exists($key,$this->arr_key_value) || count($this->arr_key_value) == 0)
+        {
             return false;
-        return $this->keyValue[$key];
+        }
+
+        return $this->arr_key_value[$key];
     }
+
+
 
     /**
      * @brief Fournit une valeur à une clé.
@@ -202,8 +264,9 @@ class Properties
      * @param $key Nom de la clé
      * @param $value Valeur de la clé
      */
-    public function setValue($key, $value){
-        $this->keyValue[$key] = $value;
+    public function setValue($key, $value)
+    {
+        $this->arr_key_value[$key] = $value;
         return true;
     }
 
